591ecc2a5624f6d4a0dd2826c355c1175a7dd5ce
Lekha Prabhakaran <168733991+lprabhakaran@users.noreply.github.com>
2024-06-06 20:27:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    var username = "TESTUSER";
+    var password = "TESTTOKEN";
+     System.out.println("Hello world!"+username+password);
   }
 }
